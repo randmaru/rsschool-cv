3775ff804f4fb4f7425fdb9e46a1302f106d73da
Alexey Ivanov <git.randmaru@ya.ru>
2021-09-21 02:49:43 +0300

    Revert "Cv html css"

--- menu.js
+++
@@ -1,6 +0,0 @@
-const nav = document.querySelector(".nav"),
-  burgerButton = document.querySelector("#burger-button");
-
-const openBurger = () => nav.classList.toggle("open-burger");
-
-burgerButton.addEventListener("click", openBurger);
